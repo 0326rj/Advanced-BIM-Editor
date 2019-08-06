0901f07cfe373e028380afd9ad86be4a337bcee4
Jaebum Kim <0326rj@gmail.com>
2019-08-07 00:25:46 +0900

    update

--- ApplicationEntry/Cmd4_Test/CmdTest.cs
+++ ApplicationEntry/Cmd4_Test/CmdTest.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using NoahDesign.Folder_WinForm;
 using System.Diagnostics;
+using BuildingCoder;
 #endregion
 
 namespace NoahDesign.Cmd4_Test
@@ -21,6 +23,8 @@ namespace NoahDesign.Cmd4_Test
   class CmdTest : IExternalCommand
   {
     #region Property
+    public UIApplication _uiapp { get; private set; }
+    public Application _app { get; private set; }
     public UIDocument _uidoc { get; private set; }
     public Document _doc { get; private set; }
 
@@ -32,7 +36,9 @@ namespace NoahDesign.Cmd4_Test
     public Result Execute( ExternalCommandData commandData,
       ref string message, ElementSet elements )
     {
-      _uidoc = commandData.Application.ActiveUIDocument;
+      _uiapp = commandData.Application;
+      _app = _uiapp.Application;
+      _uidoc = _uiapp.ActiveUIDocument;
       _doc = _uidoc.Document;
 
       var id = _uidoc.Selection.PickObject( ObjectType.Element ).ElementId;
@@ -46,10 +52,13 @@ namespace NoahDesign.Cmd4_Test
         {
           Floor floor = _doc.GetElement( id ) as Floor;
           var topFace = HostObjectUtils.GetTopFaces( floor );
-
+           
           face = _doc
             .GetElement( topFace[0] )
-            .GetGeometryObjectFromReference( topFace[0] ) as Face;      
+            .GetGeometryObjectFromReference( topFace[0] ) as Face;
+
+          FaceAnalyzer( _doc, floor );
+
         }
         TaskDialog.Show( "...", face.Area.ToString() );
       }
@@ -63,11 +72,10 @@ namespace NoahDesign.Cmd4_Test
     }
 
 
-    private bool FaceAnalyzer( Document doc, Floor floor )
+    private void FaceAnalyzer( Document doc, Floor floor )
     {
-      bool flag = false;
-  
       var refe = HostObjectUtils.GetTopFaces( floor );
+
       var singleFace = doc
         .GetElement( refe[0] )
         .GetGeometryObjectFromReference( refe[0] ) as Face;
@@ -81,21 +89,23 @@ namespace NoahDesign.Cmd4_Test
         foreach ( var crv in crvs )
         {
           if ( crv is Line )
-          {
             lines.Add( crv as Line );
-          }
         }
       }
 
       if ( lines != null )
       {
+        var ptList = new List<XYZ>();
+
         foreach ( var line in lines )
         {
-          XYZ pt1 = line.GetEndPoint( 0 );
-          XYZ pt2 = line.GetEndPoint( 1 );
-
-
+          XYZ p1 = line.GetEndPoint( 0 );
+          XYZ p2 = line.GetEndPoint( 1 );
+          var midpt = Util.Midpoint( p1, p2 );
+          ptList.Add( midpt );
         }
+
+        TaskDialog.Show( "...", ptList.Count.ToString() );
       }
     }
   }
